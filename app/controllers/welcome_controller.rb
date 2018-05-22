931a369a1e2c2cf9a25f20c8b151b831e27fcae1
Sean Bailey <sean@seanbailey.io>
2018-05-22 21:05:49 +1000

    [v0.3.11-indev] Added hero banners. And general aesthetic clean up.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,18 @@
+require 'koala'
+
 class WelcomeController < ApplicationController
 
   # GET /
   def index
     @season = Season.where('start_at < ? and end_at > ?', Time.now, Time.now).first
     @recent_games = Game.all.first(12)
+
+    # Get facebook images
+    # graph = Koala::Facebook::API.new
+    # @images = graph.get_connection('173011793321380', 'photos', {
+    #   limit: 5,
+    #   fields: ['images'],
+    # }).raw_response
   end
 
   def privacy_policy
