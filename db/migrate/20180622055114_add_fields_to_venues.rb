1885de8a7cebd36e3aea7ba44f8e88938a39b4fb
Sean Bailey <sean@seanbailey.io>
2018-09-22 21:57:30 +1000

    Fixed various SCSS issues

--- db/migrate/20180622055114_add_fields_to_venues.rb
+++ db/migrate/20180622055114_add_fields_to_venues.rb
@@ -2,23 +2,29 @@
 
 class AddFieldsToVenues < ActiveRecord::Migration[5.2]
   def change
+    reversible do |change|
+      change.up do
+        change_table :venues, bulk: true do |t|
+          t.decimal :latitude,  precision: 10, scale: 6
+          t.decimal :longitude, precision: 10, scale: 6
+          t.string :address
+          t.string :suburb
+          t.integer :state, limit: 1
+        end
+      end
+
+      change.down do
+        change_table :venues, bulk: true do |t|
+          t.remove :latitude
+          t.remove :longitude
+          t.remove :address
+          t.remove :suburb
+          t.remove :state
+        end
+      end
+    end
+
     change_table :venues, bulk: true do |t|
-      dir.up do
-        t.decimal :latitude,  precision: 10, scale: 6
-        t.decimal :longitude, precision: 10, scale: 6
-        t.string :address
-        t.string :suburb
-        t.integer :state, limit: 1
-      end
-
-      dir.down do
-        t.remove :latitude
-        t.remove :longitude
-        t.remove :address
-        t.remove :suburb
-        t.remove :state
-      end
-
       t.string :facebook
       t.string :website
       t.string :phone_number
