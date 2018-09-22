8ebf880986380c825f8765806725d91f9c7005a3
Sean Bailey <sean@seanbailey.io>
2018-09-22 22:04:28 +1000

    Fixed a migrations problem.

--- db/migrate/20180622055114_add_fields_to_venues.rb
+++ db/migrate/20180622055114_add_fields_to_venues.rb
@@ -2,29 +2,12 @@
 
 class AddFieldsToVenues < ActiveRecord::Migration[5.2]
   def change
-    reversible do |change|
-      change.up do
-        change_table :venues, bulk: true do |t|
-          t.decimal :latitude,  precision: 10, scale: 6
-          t.decimal :longitude, precision: 10, scale: 6
-          t.string :address
-          t.string :suburb
-          t.integer :state, limit: 1
-        end
-      end
-
-      change.down do
-        change_table :venues, bulk: true do |t|
-          t.remove :latitude
-          t.remove :longitude
-          t.remove :address
-          t.remove :suburb
-          t.remove :state
-        end
-      end
-    end
-
     change_table :venues, bulk: true do |t|
+      t.remove :latitude
+      t.remove :longitude
+      t.remove :address
+      t.remove :suburb
+      t.remove :state
       t.string :facebook
       t.string :website
       t.string :phone_number
