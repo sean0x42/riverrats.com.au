0bc60f1613608a87ca4f659257f13ca79a670b49
Sean Bailey <sean@seanbailey.io>
2018-09-23 20:36:51 +1000

    You can now add tournament directors

--- app/javascript/packs/game_editor.js
+++ app/javascript/packs/game_editor.js
@@ -1,17 +1,23 @@
 import { Flash } from "./components/flashes/flash";
 
-let playersQueue = [];
+let queues = {
+  player: [],
+  referee: []
+};
 
 /**
- * Determines whether the player has already been queued up.
- * @param player Player to check for.
- * @returns {boolean} Whether the player has been queued.
+ * Whether a particular queue contains a plsyer.
+ * @param q Queue to search.
+ * @param id Target player id
+ * @return {boolean} Whether the player was found.
  */
-const playerQueueContains = (player) => {
+const contains = (q, id) => {
+  const queue = queues[q];
+
   // Search for player
-  for (let i = 0; i < playersQueue.length; i++) {
-    const p = playersQueue[i];
-    if (p.id === player.id) {
+  for (let i = 0; i < queue.length; i++) {
+    const player = queue[i];
+    if (player.id === id) {
       return true;
     }
   }
@@ -20,16 +26,17 @@ const playerQueueContains = (player) => {
 };
 
 /**
- * Visually updates the players list to reflect the current state of the queue.
+ * Updates the given list to reflect the current state of the queue.
+ * @param scope A queue of players to reflect.
+ * @param list A list to redraw.
+ * @param create A function which creates a single player.
  */
-const updatePlayersList = () => {
-  // init
-  const list = document.querySelector("#js-game-players");
+const update = (scope, list, create) => {
   const emptyState = list.querySelector(".empty-state");
   let players = list.querySelector("ul.players");
 
   // Handle empty state
-  if (playersQueue.length === 0) {
+  if (queues[scope].length === 0) {
     emptyState.style.display = "block";
     if (players != null) {
       players.parentNode.removeChild(players);
@@ -45,15 +52,27 @@ const updatePlayersList = () => {
     players.classList.add("players");
     list.appendChild(players);
   } else {
-    // Remove children
+    // Remove pre-existing children
     while (players.firstChild) {
       players.removeChild(players.firstChild);
     }
   }
 
   // Create players
-  for (let i = 0; i < playersQueue.length; i++) {
-    const player = playersQueue[i];
+  for (let i = 0; i < queues[scope].length; i++) {
+    const player = queues[scope][i];
+    players.appendChild(create(player));
+  }
+};
+
+/**
+ * Visually updates the players list to reflect the current state of the queue.
+ */
+const updatePlayersList = () => {
+  // init
+  const list = document.querySelector("#js-game-players");
+  update("player", list, (player) => {
+    // Create element
     const element = document.createElement("li");
     element.setAttribute("data-player-id", player.id);
 
@@ -63,43 +82,78 @@ const updatePlayersList = () => {
     // Construct remove button
     const button = document.createElement("button");
     button.classList.add("player-remove");
+    button.setAttribute("type", "button");
     button.innerHTML = `<svg class="material-icons" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24"><path d="M18.3,5.71L18.3,5.71c-0.39-0.39-1.02-0.39-1.41,0L12,10.59L7.11,5.7c-0.39-0.39-1.02-0.39-1.41,0l0,0c-0.39,0.39-0.39,1.02,0,1.41L10.59,12L5.7,16.89c-0.39,0.39-0.39,1.02,0,1.41h0c0.39,0.39,1.02,0.39,1.41,0L12,13.41l4.89,4.89c0.39,0.39,1.02,0.39,1.41,0l0,0c0.39-0.39,0.39-1.02,0-1.41L13.41,12l4.89-4.89C18.68,6.73,18.68,6.09,18.3,5.71z"></path></svg>`;
     element.appendChild(button);
 
-    players.appendChild(element);
-  }
+    return element;
+  });
 };
 
 /**
- * Binds to various events on the standard player input.
- * @param input Input wrapper to bind to.
+ * Visually updates the referee (tournament director) list to reflect the current state of the queue.
  */
-const bindToPlayerInput = (input) => {
-  input.addEventListener("suggestion:select", (event) => {
-    const { detail } = event;
+const updateRefereesList = () => {
+  const list = document.querySelector("#js-game-referees");
+  update("referee", list, (player) => {
+    // Create element
+    const element = document.createElement("li");
+    element.setAttribute("data-player-id", player.id);
 
-    // Check if the player has already been added
-    if (playerQueueContains(detail)) {
-      new Flash("error", "Player already added", `You've already added ${detail.username} to this game.`).show();
+    // Add player
+    element.appendChild(player.asElement());
+
+    // Construct remove button
+    const button = document.createElement("button");
+    button.classList.add("player-remove");
+    button.setAttribute("type", "button");
+    button.innerHTML = `<svg class="material-icons" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24"><path d="M18.3,5.71L18.3,5.71c-0.39-0.39-1.02-0.39-1.41,0L12,10.59L7.11,5.7c-0.39-0.39-1.02-0.39-1.41,0l0,0c-0.39,0.39-0.39,1.02,0,1.41L10.59,12L5.7,16.89c-0.39,0.39-0.39,1.02,0,1.41h0c0.39,0.39,1.02,0.39,1.41,0L12,13.41l4.89,4.89c0.39,0.39,1.02,0.39,1.41,0l0,0c0.39-0.39,0.39-1.02,0-1.41L13.41,12l4.89-4.89C18.68,6.73,18.68,6.09,18.3,5.71z"></path></svg>`;
+    element.appendChild(button);
+
+    return element;
+  });
+};
+
+/**
+ * Binds to various events.
+ * @param input Input to bind to.
+ * @param scope A queue of related elements.
+ * @param updateFunc A function to update the queue.
+ */
+const bind = (input, scope, updateFunc) => {
+  input.addEventListener("suggestion:select", (event) => {
+    const player = event.detail;
+    if (contains(scope, player.id)) {
+      new Flash("error", "Player already added", `You've already added ${player.username} to this game.`).show();
       return;
     }
 
-    playersQueue.push(detail);
-    updatePlayersList();
-  }, false);
+    queues[scope].push(player);
+    updateFunc();
+  });
 };
 
 /**
  * Initialises the page by binding to event listeners (if relevant elements exist).
- * @param event Tubrolinks load event.
  */
-const init = (event) => {
+const init = () => {
+  // Handle game players
   const playerInput = document.querySelector("#js-game-players-input");
   if (playerInput !== null) {
-    bindToPlayerInput(playerInput);
+    bind(playerInput, "player", updatePlayersList);
   }
 
-  playersQueue = [];
+  // Handle game referees
+  const refereeInput = document.querySelector("#js-game-referees-input");
+  if (refereeInput !== null) {
+    bind(refereeInput, "referee", updateRefereesList);
+  }
+
+  // Reset
+  queues = {
+    player: [],
+    referee: []
+  };
 };
 
 document.addEventListener("turbolinks:load", init);
