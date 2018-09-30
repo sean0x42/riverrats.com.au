c1122799bc4de3958fe1989cb85e93985ecffcb1
Sean Bailey <sean@seanbailey.io>
2018-10-01 00:54:32 +1000

    Added master key to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ end
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml'
+append :linked_files, 'config/database.yml', 'config/master.keycre'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
